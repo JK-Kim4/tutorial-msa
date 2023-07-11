b2651d031b85ed7773faf225e306e77aeaa6153b
jongwan <jw.kim@changbi.com>
2023-07-11 17:13:07 +0900

    msa tutorial first init

---
+++ micro-service-front/src/test/java/com/jw/microservicesfront/MicroServicesFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jw.microservicesfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServicesFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

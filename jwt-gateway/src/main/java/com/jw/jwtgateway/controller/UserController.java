e68fe5f7fbdeba96844ce7aa7cc73bf14a36d862
jongwan <jongbell4@gmail.com>
2023-07-20 17:42:46 +0900

    feat: msa front controller feat: 사용자 검증 Controller

--- jwt-gateway/src/main/java/com/jw/jwtgateway/controller/UserController.java
+++ jwt-gateway/src/main/java/com/jw/jwtgateway/controller/UserController.java
@@ -4,10 +4,8 @@ import com.jw.jwtgateway.controller.dto.UserDto;
 import com.jw.jwtgateway.entity.User;
 import com.jw.jwtgateway.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -25,7 +23,18 @@ public class UserController {
     public ResponseEntity<User> signup(
             @Valid @RequestBody UserDto dto){
         return ResponseEntity.ok(userService.signup(dto));
+    }
 
+    @GetMapping("/user")
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+    public ResponseEntity<User> getMyUserInfo(){
+        return ResponseEntity.ok(userService.getMyuserWithAuthorities().get());
+    }
+
+    @GetMapping("/user/{username}")
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    public ResponseEntity<User> getUserInfo(@PathVariable String username){
+        return ResponseEntity.ok(userService.getUserWithAuthorities(username).get());
     }
 
 }
